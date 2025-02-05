f82c03def9c74ae9f507324bbc8a2e0ec63cfd5d
abdulmajid <abdulmajid08180881@gmail.com>
2025-02-05 16:36:29 +0700

    done: added restful api resource CRUD Users

--- project-app/app/Http/Controllers/UserController.php
+++ project-app/app/Http/Controllers/UserController.php
@@ -5,11 +5,56 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Resources\UserResource;
 use App\Models\User;
+use Illuminate\Foundation\Validation\ValidatesRequests;
 
 class UserController extends Controller
 {
+    use ValidatesRequests;
+
+    // show list users
     public function index()
     {
         return UserResource::collection(User::all());
+    }
+
+    // create users
+    public function store(Request $request)
+    {
+        try {
+            $validatedData = $request->validate([
+                'username' => 'required|max:12|unique:users,username',
+                'password' => 'required|min:6',
+                'nama_akun' => 'required',
+                'role' => 'required',
+                'status' => 'required',
+            ]);
+            $user = User::create([
+                'username' => $validatedData['username'],
+                'password' => bcrypt($validatedData['password']),
+                'nama_akun' => $validatedData['nama_akun'],
+                'role' => $validatedData['role'],
+                'status' => $validatedData['status'],
+            ]);
+
+            return new UserResource($user);
+        } catch (\Illuminate\Validation\ValidationException $err) {
+            return response()->json(['error' => 'Validation failed', 'messages' => $err->errors()], 422);
+        }
+    }
+
+    // show user by id
+    public function show(User $user)
+    {
+        return new UserResource($user);
+    }
+
+    // update users
+    public function update(Request $request, User $user) {}
+
+    // delete users
+    public function destroy(User $user)
+    {
+        $user->delete();
+        return response()->json(['message' => 'User deleted successfully']);
     }
 }
--- project-app/routes/api.php
+++ project-app/routes/api.php
@@ -8,12 +8,15 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('v1/kas-app')->group(function () {
     // Public Routes
     // Route::post('/register', [AuthController::class, 'register']);
-    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/auth/login', [AuthController::class, 'login']);
 
     // Protected Routes (Middleware)
     Route::middleware('auth:sanctum')->group(function () {
-        Route::post('/logout', [AuthController::class, 'logout']);
-        Route::get('/users', [UserController::class, 'index'])
+        Route::post('/auth/logout', [AuthController::class, 'logout']);
+        // Route::get('/users', [UserController::class, 'index'])
+        //     ->middleware([RoleMiddleware::class . ':admin,ketua_kelas']);
+        Route::apiResource('/users', UserController::class)
+            // ->except(['store', 'update', 'destroy']) = pengecualian untuk menggunakan role pada route berikut ini
             ->middleware([RoleMiddleware::class . ':admin,ketua_kelas']);
     });
 });
